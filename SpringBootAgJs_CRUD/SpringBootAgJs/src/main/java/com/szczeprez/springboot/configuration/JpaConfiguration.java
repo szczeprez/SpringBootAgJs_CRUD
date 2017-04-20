cb599fcf8cc2852104603af03190e073621156f2
szczeprez <szczeprez@poczta.fm>
2017-04-20 22:18:45 +0200

    Modificated application.yml and JpaConfiguration

--- SpringBootAgJs_CRUD/SpringBootAgJs/src/main/java/com/szczeprez/springboot/configuration/JpaConfiguration.java
+++ SpringBootAgJs_CRUD/SpringBootAgJs/src/main/java/com/szczeprez/springboot/configuration/JpaConfiguration.java
@@ -1,9 +1,24 @@
 package com.szczeprez.springboot.configuration;
 
+import java.util.Properties;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -13,8 +28,49 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class JpaConfiguration {
 
+	@Autowired
+	private Environment environment;
+
+	@Value("${datasource.sampleapp.maxPoolSize:10}")
+	private int maxPoolSize;
+	
 	@Bean
+	@Primary
+	@ConfigurationProperties(prefix = "datasource.sampleapp")
 	public DataSourceProperties dataSourceProperties(){
 		return new DataSourceProperties(); 
+	}
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws NamingException {
+		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		factoryBean.setDataSource(dataSource());
+		factoryBean.setPackagesToScan(new String[] { "com.szczeprez.springboot.model" });
+		factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+		factoryBean.setJpaProperties(jpaProperties());
+		return factoryBean;
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		 return DataSourceBuilder.create().build();
+	}
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+		return hibernateJpaVendorAdapter;
+	}
+	
+	private Properties jpaProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", environment.getRequiredProperty("datasource.sampleapp.hibernate.dialect"));
+		properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("datasource.sampleapp.hibernate.hbm2ddl.method"));
+		properties.put("hibernate.show_sql", environment.getRequiredProperty("datasource.sampleapp.hibernate.show_sql"));
+		properties.put("hibernate.format_sql", environment.getRequiredProperty("datasource.sampleapp.hibernate.format_sql"));
+		if(StringUtils.isNotEmpty(environment.getRequiredProperty("datasource.sampleapp.defaultSchema"))){
+			properties.put("hibernate.default_schema", environment.getRequiredProperty("datasource.sampleapp.defaultSchema"));
+		}
+		return properties;
 	}
 }
