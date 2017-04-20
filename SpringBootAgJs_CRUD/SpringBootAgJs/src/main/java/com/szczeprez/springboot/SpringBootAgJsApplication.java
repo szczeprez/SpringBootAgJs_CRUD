46b84564b3d4041718a783198cf3472ade6dd3c5
szczeprez <szczeprez@poczta.fm>
2017-04-20 21:37:28 +0200

    first basic commit

---
+++ SpringBootAgJs_CRUD/SpringBootAgJs/src/main/java/com/szczeprez/springboot/SpringBootAgJsApplication.java
@@ -0,0 +1,12 @@
+package com.szczeprez.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAgJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAgJsApplication.class, args);
+	}
+}
